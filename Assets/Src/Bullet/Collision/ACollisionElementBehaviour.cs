e632539234057f4ccf4019ab009e31edf8e893aa
kino1022 <yuta_fujita@outlook.com>
2025-10-27 14:43:36 +0900

    update

---
+++ Assets/Src/Bullet/Collision/ACollisionElementBehaviour.cs
@@ -0,0 +1,65 @@
+using RinaBullet.Collision;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using Src.Utility;
+using UnityEngine;
+using VContainer;
+
+namespace Src.Bullet.Collision {
+
+    enum Timing {
+        OnEnter,
+        OnExit,
+    }
+    
+    public abstract class ACollisionElementBehaviour : SerializedMonoBehaviour, ICollisionCallBackElement {
+
+        [SerializeField] 
+        [LabelText("処理の優先度")] 
+        private int m_priority = 0;
+        
+        [OdinSerialize]
+        [LabelText("発動タイミング")]
+        private Timing m_timing = Timing.OnEnter;
+        
+        protected IObjectResolver m_resolver;
+
+        public int Priority => m_priority;
+
+        [Inject]
+        public void Construct(IObjectResolver resolver) {
+            m_resolver = resolver;
+        }
+        
+        private void Start() {
+            
+            RegisterCollisionManager();
+            
+            ResolveDependence();
+        }
+
+        public abstract void OnCollisionEnterCallBack(UnityEngine.Collision other);
+
+        protected virtual void ResolveDependence () {}
+
+        private void RegisterCollisionManager() {
+            
+            var mana = ComponentsUtility.GetComponentsFromWhole<ICollisionCallBackManager>(gameObject);
+
+            if (mana is null) {
+                enabled = false;
+                return;
+            }
+
+            switch (m_timing) {
+                case Timing.OnEnter:
+                    mana.AddOnCollision(this);
+                    break;
+                case Timing.OnExit:
+                    mana.RemoveOnCollision(this);
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ Assets/Src/Bullet/Collision/InstancePrefabOnCollision.cs
@@ -0,0 +1,23 @@
+using System;
+using RinaBullet.Collision;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Src.Bullet.Collision {
+    public class InstancePrefabOnCollision : ACollisionElementBehaviour {
+        
+        [SerializeField]
+        [LabelText("生成プレファブ")]
+        private GameObject m_prefab;
+
+        public override void OnCollisionEnterCallBack(UnityEngine.Collision other) {
+            m_resolver.Instantiate(
+                m_prefab,
+                other.contacts[0].normal,
+                Quaternion.identity
+            );
+        }
+    }
+}
